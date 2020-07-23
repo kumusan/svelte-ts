95516944ef5a5c39c3d445442181f0f3df593bc7
kumusan <ryogi0200@gmail.com>
2020-07-23 21:20:13 +0900

    💖TS

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Header from "./Header.svelte";
+</script>
+
+<main>
+  <!---
+		Error: Type 'number' is not assignable to type 'string'. (ts)
+		<Header name={1} />
+	-->
+  <Header name={'test'} />
+  <p>Svelte ❌ Typescript</p>
+</main>
---
+++ src/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<header>
+  <h1>Hello {name}</h1>
+</header>
